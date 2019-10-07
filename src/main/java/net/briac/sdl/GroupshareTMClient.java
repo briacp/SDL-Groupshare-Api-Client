2f37afb99c7296389320b8429fe60a68dcee013a
Briac Pilpré <briacp@gmail.com>
2019-10-07 09:18:27 +0200

    Fix license text position

--- src/main/java/net/briac/sdl/GroupshareTMClient.java
+++ src/main/java/net/briac/sdl/GroupshareTMClient.java
@@ -1,4 +1,23 @@
+/**************************************************************************
+SDLApiClient - GroupShare Web API
 
+Copyright (C) 2019 Briac Pilpré
+Home page: http://www.omegat.org/
+Support center: http://groups.yahoo.com/group/OmegaT/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
 package net.briac.sdl;
 
 import java.io.FileNotFoundException;
@@ -42,26 +61,7 @@ import net.briac.sdl.model.ParagraphUnit;
 import net.briac.sdl.model.SystemFields;
 import net.briac.sdl.model.TranslationUnits;
 
-/**************************************************************************
-SDLApiClient - GroupShare Web API
 
-Copyright (C) 2019 Briac Pilpré
-Home page: http://www.omegat.org/
-Support center: http://groups.yahoo.com/group/OmegaT/
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************/
 public class GroupshareTMClient extends DefaultHandler {
 
     private String sourceLanguage;
